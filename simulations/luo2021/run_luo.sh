49f65fa3318f79bbb5b1eb21924e340e4b496375
Ashak Umesh <ashakumesh2020@gmail.com>
2026-06-18 14:29:25 +0530

    Add threshold evasion attack evidence and results

--- simulations/luo2021/run_luo.sh
+++ simulations/luo2021/run_luo.sh
@@ -2,7 +2,7 @@
 
 ./out/clang-release/luo2021 \
 -u Cmdenv \
--c GCLPhaseAttack \
+-c ThresholdEvasionAttack \
 -n .:/home/ashakumesh/research/omnetpp-6.4.0/samples/inet/src \
 -l /home/ashakumesh/research/omnetpp-6.4.0/samples/inet/src/libINET.so \
 omnetpp.ini
